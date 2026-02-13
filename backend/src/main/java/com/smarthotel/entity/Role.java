90f3daca13d8d62ff88d7a526e55fdcfa54eb9b0
MohanKrishna <uppathimohankrishnareddy23@ifheindia.org>
2026-02-13 20:28:08 +0530

    Create Role.java entity class with JPA annotations and Lombok

---
+++ backend/src/main/java/com/smarthotel/entity/Role.java
@@ -0,0 +1,18 @@
+package com.smarthotel.entity;
+
+import javax.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
